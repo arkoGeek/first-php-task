1424bdda2e20b7727a0414d7c1e7f0c354ddf5db
Arko Antony Mitra <arko@geekyants.com>
2022-07-11 12:16:55 +0530

    First PHP task

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+  <form method="post" action="submit.php">
+    <label for="name">Enter your name : </label>
+    <input type="text" name="name" id="name">
+    <input type="submit" value="SUBMIT">
+  </form>
+</body>
+</html>   
---
+++ submit.php
@@ -0,0 +1,5 @@
+<?php
+  if(isset($_POST["name"])){
+    echo "Hey " . $_POST["name"] . ", nice to meet you. I am PHP.";
+  }
+?>
